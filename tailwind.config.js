9cab88cb90599db09692c33f8191ca1ede9ff814
hejin8307 <hejin8307@naver.com>
2024-04-27 17:15:04 +0900

    Design: make kiosk screen

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        starbucksGreen: "#00754A",
+        starbucksBeige: "#F2F0EB",
+        danger: "#DC3545",
+        primary: "0000FF",
+      },
+    },
+  },
+  plugins: [],
+};
