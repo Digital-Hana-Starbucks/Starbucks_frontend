58e86307a169d1e2dda8d7e9e19898a57f3d3da0
hejin8307 <hejin8307@naver.com>
2024-04-28 01:49:20 +0900

    Chore: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Landing from "./pages/Landing";
+import CheckUser from "./pages/CheckUser";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "checkUser", element: <CheckUser /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
