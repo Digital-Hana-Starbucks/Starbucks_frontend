097fc0b939d63cd32edd1206225170cbbd58d3ec
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-05-09 14:48:26 +0900

    Conflict: baseUrl충돌 해결

--- src/apis/url.ts
+++ src/apis/url.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export const API_BASE_URL = "http://52.2.44.62:8080/api/v1";
 // export const API_BASE_URL = "http://localhost:8080/api/v1";
-=======
-// export const API_BASE_URL = "http://52.2.44.62:8080/api/v1";
-export const API_BASE_URL = "http://localhost:8080/api/v1";
->>>>>>> 8d3faf5d1a726ab575eb6f44cdb3252662aaab52
